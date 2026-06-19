3562e97e05ba1d06cc71750eec7b4671d5e0617c
Herlon378 <herlonreis15@gmail.com>
2026-06-19 12:03:33 -0400

    Refactor WebSocket server implementation
    Refactor WebSocket server setup and improve message handling.

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
-
-const express = require('express');
-const { Server } = require('ws');
-
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
 
-// Cria um mini app express para lidar com o tráfego seguro do Render
-const app = express();
-const server = app.listen(PORT, () => console.log(`Servidor WebRTC rodando na porta ${PORT}`));
+const wss = new WebSocket.Server({ port: PORT });
 
-// Conecta o WebSocket no servidor seguro
-const wss = new Server({ server });
+console.log(`Servidor de Áudio rodando na porta ${PORT}`);
 
 wss.on('connection', (ws) => {
-    console.log('Novo dispositivo conectado ao rádio com segurança!');
-
     ws.on('message', (message) => {
-        // Encaminha os pacotes do WebRTC para os celulares conectados
+        // Repassa os blocos de áudio diretamente para os outros celulares
         wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === 1) {
-
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
         });
     });
-
-    ws.on('close', () => console.log('Dispositivo desconectado.'));
 });
